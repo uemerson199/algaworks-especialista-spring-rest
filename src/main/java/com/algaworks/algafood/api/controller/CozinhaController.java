72348aef109d88d094403312bfdbece63c0232fb
Uemerson Ribeiro <uemersonribeiro220@gmail.com>
2024-12-05 09:56:18 -0300

    Manipulando a Resposta HTTP com ResponseEntity

--- src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
+++ src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -3,12 +3,13 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.api.model.CozinhasXmlWrapper;
@@ -34,10 +35,21 @@ public class CozinhaController {
 		return new CozinhasXmlWrapper(repository.listar());
 	}
 	
-	@ResponseStatus(value = HttpStatus.CREATED)
+	
 	@GetMapping(value = "/{id}")
-	public Cozinha buscar(@PathVariable Long id) {
-		return repository.buscar(id);
+	public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
+		Cozinha cozinha = repository.buscar(id);
+		//return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+		//return ResponseEntity.ok(cozinha);
+		
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas");
+		
+		
+		return ResponseEntity
+				.status(HttpStatus.FOUND)
+				.headers(headers)
+				.build();
 	}
 	
 
