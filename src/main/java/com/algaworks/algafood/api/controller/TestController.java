f20ea8ad40507077443252f96e38da94de3524a8
Uemerson Ribeiro <uemersonribeiro220@gmail.com>
2024-12-11 17:15:27 -0300

    Criando consultas com query methods

--- src/main/java/com/algaworks/algafood/api/controller/TestController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TestController.java
@@ -1,9 +1,15 @@
 package com.algaworks.algafood.api.controller;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.domain.model.Cozinha;
 import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
 @RestController
@@ -13,9 +19,14 @@ public class TestController {
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
 	
-//	@GetMapping("/cozinhas/por-nome")
-//	public List<Cozinha> cozinhasPorNome(@RequestParam String nome) {
-//		return cozinhaRepository.consultarPorNome(nome);
-//	}
-//	
+	@GetMapping("/cozinhas/por-nome")
+	public List<Cozinha> cozinhasPorNome(@RequestParam String nome) {
+		return cozinhaRepository.findTodasBynome(nome);
+	}
+	
+	@GetMapping("/cozinhas/por-nome-unico")
+	public Optional<Cozinha> cozinhaPorNome(@RequestParam String nome) {
+		return cozinhaRepository.findByNome(nome);
+	}
+	
 }
--- src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -1,5 +1,8 @@
 package com.algaworks.algafood.domain.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +12,8 @@ import com.algaworks.algafood.domain.model.Cozinha;
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 
 	
-	//List<Cozinha> consultarPorNome(String nome);
+	List<Cozinha> findTodasBynome(String nome);
 	
+	Optional<Cozinha> findByNome(String nome);
 	
 }
