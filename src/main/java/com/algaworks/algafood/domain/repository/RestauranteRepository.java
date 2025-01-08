f7d7a9f48937124b1d7ed44f70b72cae45ddaf05
Uemerson Ribeiro <uemersonribeiro220@gmail.com>
2025-01-08 11:08:07 -0300

    Resolvendo o Problema do N+1 com fetch join na JPQL

--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import com.algaworks.algafood.domain.model.Restaurante;
@@ -13,6 +14,9 @@ import com.algaworks.algafood.domain.model.Restaurante;
 public interface RestauranteRepository 
 		extends CustomJpaRepository<Restaurante, Long>, ResturanteRepositoryQueries,
 		JpaSpecificationExecutor<Restaurante>{
+	
+	@Query("from Restaurante r join fetch r.cozinha left join fetch r.formaPagamentos")
+	List<Restaurante> findAll();
 	
 	List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 	
