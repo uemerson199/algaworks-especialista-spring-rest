e31ac04b7202a67414b1d74601c63dafe5d9be23
Uemerson Ribeiro <uemersonribeiro220@gmail.com>
2025-01-30 16:37:32 -0300

    Desafio: tratando outras exceções não capturadas

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 	
+	ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema"),
 	RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado"),
 	PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido"),
 	MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível"),
