2b7bdd97aea9f74dabffbd7bb28da3c115134259
Uemerson Ribeiro <uemersonribeiro220@gmail.com>
2025-03-10 14:59:29 -0300

    Customizando o mapeamento de propriedades com ModelMapper

--- src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
@@ -12,4 +12,16 @@ public class ModelMapperConfig {
 		return new ModelMapper();
 	}
 
+
+//	@Bean
+//	public ModelMapper modelMapper() {
+//		 var modelMapper = new ModelMapper();
+//		 
+//		 modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
+//		 	  .addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
+//		 
+//		 return modelMapper;
+//	}
+
+	
 }
